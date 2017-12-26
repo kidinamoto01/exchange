077eaec284932eb01ad755582ec9d895eddc75ec
kidinamoto01 <kidinamoto@qq.com>
2017-12-26 10:34:51 +0800

    add truffle test for transfer

--- practice/test/01_fixed_supply_token_test.js
+++ practice/test/01_fixed_supply_token_test.js
@@ -39,5 +39,41 @@ contract('MyToken', function(accounts) {
             assert.equal(accountBalance.toNumber(), 0, "No token is owned by the second account");
         });
     });
+    
+     it("should send token correctly", function() {
+        var token;
+
+        //    Get initial balances of first and second account.
+        var account_one = accounts[0];
+        var account_two = accounts[1];
+
+        var account_one_starting_balance;
+        var account_two_starting_balance;
+        var account_one_ending_balance;
+        var account_two_ending_balance;
+
+        var amount = 10;
+
+        return fixedSupplyToken.deployed().then(function(instance) {
+            token = instance;
+            return token.balanceOf.call(account_one);
+        }).then(function(balance) {
+            account_one_starting_balance = balance.toNumber();
+            return token.balanceOf.call(account_two);
+        }).then(function(balance) {
+            account_two_starting_balance = balance.toNumber();
+            return token.transfer(account_two, amount, {from: account_one});
+        }).then(function() {
+            return token.balanceOf.call(account_one);
+        }).then(function(balance) {
+            account_one_ending_balance = balance.toNumber();
+            return token.balanceOf.call(account_two);
+        }).then(function(balance) {
+            account_two_ending_balance = balance.toNumber();
+
+            assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
+            assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
+        });
+    });
 
 });
